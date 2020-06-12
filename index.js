d0377484f1d9050b93f965dea79d9ad27dd13540
Yaldar <yaldar.hapak@gmai.com>
2020-06-12 14:05:53 +0200

    done

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 // Option 2 - jQuery Smooth Scrolling
-$('.pic').on('click', (event) => {
+$('.pic').on('click', event => {
   event.preventDefault();
-  const hash = '#' + event.target.className.split(" ")[1]
-  $('html, body')
-    .animate({
-      scrollTop: $(hash).offset().top - 250
-    }, 700);
-
+  const hash = '#' + event.target.className.split(' ')[1];
+  $('html, body').animate(
+    {
+      scrollTop: $(hash).offset().top - 250,
+    },
+    700,
+  );
 });
 
-$('.test').hide()
+// $('.bio').hide()
 
-$('.pic').on('mouseover', e => {
-  $('').slideDown()
-})
+// $(window).scroll(() => {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     $('header').animate({ height: '60px' }, 400);
+//     // document.getElementsByTagName('header')[0].style.height= '60px';
+//   } else {
+//     $('header').animate({ height: '160px' }, 400);
 
-
-$('.pic').on('mouseleave', e => {
-  $('.minibio').slideUp()
-})
-
-
-
-window.addEventListener('scroll', (e) => {
-
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementsByTagName("header")[0].setAttribute('style', 'padding: 30px 10px')
-
-    console.log(document.getElementsByTagName("header")[0].style);
-
-    // document.getElementById("logo").style.fontSize = "25px";
-
-  }
-  // } else {
-  //   document.getElementsByTagName("header").style.padding = "80px 10px";
-  //   document.getElementById("logo").style.fontSize = "35px";
-  // }
-});
+//     // document.getElementsByTagName('header')[0].style.height= '180px';
+//   }
+// });
